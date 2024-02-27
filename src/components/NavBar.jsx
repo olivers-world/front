fef80f141515ed00a665e39ee9b872b82079d693
Synaptique <garibaldialexis@gmail.com>
2024-02-27 20:26:35 +0100

    Navbar centered

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,15 +5,11 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
   return (
     <div className="absolute z-20 w-full flex justify-evenly items-center text-white my-3">
-      <NavLink to="/" className="text-2xl font-jacqueFrancois">Accueil</NavLink>
-      <NavLink to="/reservation" className="text-2xl font-jacqueFrancois">Réservation</NavLink>
-      
-      <NavLink to="/">
-        <img src={logo} alt="logo"/>
-      </NavLink> 
-      
-      <NavLink to="/menu" className="text-2xl font-jacqueFrancois">Menu</NavLink>
-      <NavLink to="/contact" className="text-2xl font-jacqueFrancois">Contact</NavLink>
+      <NavLink to="/" className="text-2xl font-jacqueFrancois -translate-x-1/2">Accueil</NavLink>
+      <NavLink to="/reservation" className="text-2xl font-jacqueFrancois -translate-x-1/2">Réservation</NavLink>
+      <NavLink to="/"><img src={logo} alt="logo" className="-translate-x-1/2"/></NavLink> 
+      <NavLink to="/menu" className="text-2xl font-jacqueFrancois -translate-x-1/2">Menu</NavLink>
+      <NavLink to="/contact" className="text-2xl font-jacqueFrancois -translate-x-1/2">Contact</NavLink>
     </div>
   );
 }
