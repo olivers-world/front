eef6d8a7fdacfee0902808cae267644403854d8d
Adam COURTARO <101117765+adamctr@users.noreply.github.com>
2024-03-17 23:26:01 +0100

    admin advanced

---
+++ src/components/Accordeon.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState } from "react";
+
+const Accordeon = ({ title, children }) => {
+  const [accordionOpen, setAccordionOpen] = useState(false);
+
+  return (
+    <div className="relative mb-3">
+      <h6 className="mb-0">
+        <button
+          className="relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-1 group "
+          data-collapse-target="collapse-1"
+          onClick={() => setAccordionOpen(accordionOpen ? false : true)}
+        >
+          <span className="uppercase">{title}</span>
+        </button>
+      </h6>
+      <div
+        data-collapse="collapse-1"
+        className={`${
+          accordionOpen ? "h-fit" : "h-0"
+        } overflow-hidden transition-all duration-300 ease-in-out`}
+      >
+        <div className="p-4 text-sm leading-normal ">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Accordeon;
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -2,34 +2,110 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import Dashboard from "../components/Dashboard";
 import Footer from "../components/Footer";
+import MakeRecipe from "@/components/MakeRecipe";
+import { useState, useEffect } from "react";
+
+const Section = ({ children, display, handleSetActiveSection, active }) => {
+  return (
+    <section
+      className={`cursor-pointer capitalize bg-white py-4 px-4 border-b 
+      ${active ? "font-medium" : ""} 
+      ${display ? "" : "hidden"}`}
+      onClick={() => handleSetActiveSection(children)}
+    >
+      {children}
+    </section>
+  );
+};
+
+const Burger = ({ expanded, handleMenu }) => {
+  return (
+    <div
+      className={`absolute text-primary ${
+        expanded ? "-rotate-90" : ""
+      } transition-all duration-500 origin-center xl:hidden right-[15%] cursor-pointer top-5 w-fit h-fit font-medium text-xl z-40`}
+      onClick={() => handleMenu()}
+    >
+      <div className="flex flex-col gap-1">
+        <div className="h-[0.15em] w-8 rounded bg-primary"></div>
+        <div className="h-[0.15em] w-8 rounded bg-primary"></div>
+        <div className="h-[0.15em] w-8 rounded bg-primary"></div>
+      </div>
+    </div>
+  );
+};
 
 const Admin = () => {
+  const [expanded, setExpanded] = useState(false);
+  const [activeSection, setActiveSection] = useState("dashboard");
+
+  useEffect(() => {
+    if (window.innerWidth > 1280) {
+      setExpanded(true);
+    }
+
+    const handleResize = () => {
+      if (window.innerWidth > 1280) {
+        setExpanded(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const handleMenu = () => {
+    setExpanded(!expanded);
+  };
+
+  const [sections, setSections] = useState([
+    { name: "dashboard", active: true },
+    { name: "confectionner un menu", active: false },
+    { name: "modifier, annuler une réservation", active: false },
+    { name: "avis", active: false },
+  ]);
+
+  const handleSetActiveSection = (name) => {
+    setSections((prevSections) =>
+      prevSections.map((section) => ({
+        ...section,
+        active: section.name === name,
+      }))
+    );
+
+    console.log("chech");
+  };
+
   return (
     <>
       <NavBar linkscolor="black" />
+
       <div className="h-[100px]"></div>
+
       <div className="mx-8 min-h-screen">
         <section className="flex flex-wrap max-w-6xl mx-auto my-8 ">
-          <aside className="border w-fit">
-            <section className="cursor-pointer py-4 px-4 border-b font-medium">
-              Dashboard
-            </section>
-            <section className="cursor-pointer py-4 px-4 border-b">
-              Confectionner un plat
-            </section>
-            <section className="cursor-pointer py-4 px-4 border-b">
-              Modifier, annuler une réservation
-            </section>
-
-            <section className="cursor-pointer py-4 px-4 border-b">
-              Avis
-            </section>
+          <aside className="relative border w-full text-center xl:w-fit">
+            <Burger handleMenu={handleMenu} expanded={expanded} />
+            {sections.map((section, index) => {
+              return (
+                <Section
+                  display={expanded || section.active}
+                  key={index}
+                  handleSetActiveSection={handleSetActiveSection}
+                  active={section.active}
+                >
+                  {section.name}
+                </Section>
+              );
+            })}
           </aside>
-          <section className="w-full py-4 px-4 max-w-[835px] border">
-            <Dashboard />
+          <section className="w-full py-4 px-4 xl:max-w-[835px]  border">
+            {/*<Dashboard /> */}
+            <MakeRecipe />
           </section>
         </section>
       </div>
+
       <Footer />
     </>
   );
