c790e8f510555eafb50c657bf2ea46845493af6c
Synaptique <garibaldialexis@gmail.com>
2024-03-22 19:17:58 +0100

    [WIP] protected admin route

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Reservation from './pages/Reservation.jsx';
-import Home from './pages/Home.jsx';
-import Contact from './pages/Contact.jsx';
-import Menu from './pages/Menu.jsx';
-import Register from './pages/Register.jsx';
-import Login from './pages/Login.jsx';
-import Admin from './pages/Admin.jsx';
-import RequireAuth from './components/RequireAuth';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Reservation from "./pages/Reservation.jsx";
+import Home from "./pages/Home.jsx";
+import Contact from "./pages/Contact.jsx";
+import Menu from "./pages/Menu.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import Admin from "./pages/Admin.jsx";
+import Layout from "./components/Layout.jsx";
+import RequireAuth from "./components/RequireAuth";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/reservation" element={<Reservation />} />
-      <Route path="/menu" element={<Menu />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/admin" element={
-        <RequireAuth allowedRoles={['Administrateur']}>
-          <Admin />
-        </RequireAuth>
-      } />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route
+          path="/admin"
+          element={
+            <RequireAuth allowedRoles={["Administrateur"]}>
+              <Admin />
+            </RequireAuth>
+          }
+        /> */}
+        <Route path="/admin" element={<Admin />} />
+      </Route>
     </Routes>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState, useEffect } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
@@ -11,6 +12,10 @@ const Login = () => {
 
   const emailRef = useRef();
   const errRef = useRef();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
 
   const [email, setEmail] = useState("");
   const [pwd, setPwd] = useState("");
@@ -45,7 +50,7 @@ const Login = () => {
       const roles = response?.data?.roles;
       // Stocker le token et les roles dans le localStorage
       localStorage.setItem("userInfo", JSON.stringify({ prenom, nom, email }));
-      localStorage.setItem("roles",roles);
+      localStorage.setItem("roles", roles);
 
       setAuth({
         email: email,
@@ -56,7 +61,7 @@ const Login = () => {
 
       setEmail("");
       setPwd("");
-      setSuccess(true);
+      navigate(from, { replace: true });
     } catch (err) {
       if (!err?.response) {
         setErrMsg("No Server Response");
@@ -82,94 +87,80 @@ const Login = () => {
   return (
     <>
       <NavBar linkscolor="black" position="block" />
-      {success ? (
-        <section className="min-h-screen flex flex-1 flex-col  px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h1>Vous êtes connecté !</h1>
-            <br />
-            <p>
-              <a href="/" className="">
-                Aller à l'accueil
-              </a>
-            </p>
-          </div>
-        </section>
-      ) : (
-        <section className=" min-h-screen flex flex-1 flex-col px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <p
-              ref={errRef}
-              // style={errMsg ? styles.errmsg : styles.offscreen}
-              aria-live="assertive"
-            >
-              {errMsg}
-            </p>
-            <h1 className="text-center font-bold text-2xl">Connexion</h1>
+      <section className=" min-h-screen flex flex-1 flex-col px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <p
+            ref={errRef}
+            // style={errMsg ? styles.errmsg : styles.offscreen}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1 className="text-center font-bold text-2xl">Connexion</h1>
 
-            <form
-              className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm"
-              onSubmit={handleSubmit}
-            >
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Email:
-                </label>
-                <input
-                  type="text"
-                  id="email"
-                  ref={emailRef}
-                  autoComplete="off"
-                  onChange={(e) => setEmail(e.target.value)}
-                  value={email}
-                  required
-                  className="mb-4 block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:primary-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Mot de Passe:
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  onChange={(e) => setPwd(e.target.value)}
-                  value={pwd}
-                  required
-                  className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:primary-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-
-              <p className="gap-1 text-xs flex mt-4 text-primary-600 w-full justify-center">
-                Pas de compte ?
-                <br />
-                <span className="">
-                  <a href="/register" className="">
-                    S'inscrire
-                  </a>
-                </span>
-              </p>
-
-              <button className="cursor-pointer mt-4 flex w-full justify-center rounded-md bg-primary-600 px-3 py-1.5 text-sm font-semibold leading-6 text-black   focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ">
-                Se connecter
-              </button>
+          <form
+            className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm"
+            onSubmit={handleSubmit}
+          >
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email:
+              </label>
               <input
-                type="checkbox"
-                id="persist"
-                onChange={togglePersist}
-                checked={persist}
+                type="text"
+                id="email"
+                ref={emailRef}
+                autoComplete="off"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+                className="mb-4 block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:primary-600 sm:text-sm sm:leading-6"
               />
-              <label htmlFor="persist">Trust This Device</label>
-            </form>
-          </div>
-        </section>
-      )}
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Mot de Passe:
+              </label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+                className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:primary-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+
+            <p className="gap-1 text-xs flex mt-4 text-primary-600 w-full justify-center">
+              Pas de compte ?
+              <br />
+              <span className="">
+                <a href="/register" className="">
+                  S'inscrire
+                </a>
+              </span>
+            </p>
+
+            <button className="cursor-pointer mt-4 flex w-full justify-center rounded-md bg-primary-600 px-3 py-1.5 text-sm font-semibold leading-6 text-black   focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ">
+              Se connecter
+            </button>
+            <input
+              type="checkbox"
+              id="persist"
+              onChange={togglePersist}
+              checked={persist}
+            />
+            <label htmlFor="persist">Trust This Device</label>
+          </form>
+        </div>
+      </section>
       <Footer />
     </>
   );
